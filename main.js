7a068a0a83a4ec430f54ec26433358ef200aa69c
Suwon Jeong <integer.won@gmail.com>
2023-07-07 15:02:36 +1200

    Update menu bar and log

---
+++ main.js
@@ -0,0 +1,17 @@
+// let menu = document.getElementById('menu')
+
+// // 마우스를 가져다 대면 메뉴 바 표시
+// menu.addEventListener('mouseenter', function () {
+//   this.style.display = 'block'
+// })
+
+// // 마우스를 벗어나면 메뉴 바 숨김
+// menu.addEventListener('mouseleave', function () {
+//   this.style.display = 'none'
+// })
+
+
+let github = document.getElementById('icon-github')
+let linkedin = document.getElementById('icon-linkedin')
+
+
