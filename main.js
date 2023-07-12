d8ec0f7ceea49c332e8633af59e7715e4a6b6600
Suwon Jeong <integer.won@gmail.com>
2023-07-12 19:08:43 +1200

    Update css

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let homeButton = document.getElementById('home')
 homeButton.addEventListener('click', (event) => {
-  window.location.href = 'https://integer-1.github.io/'
+  window.location.href = '../index.html'
 })
 
 let github = document.getElementById('icon-github')
@@ -15,14 +15,14 @@ linkedin.addEventListener('click', (event) => {
   window.location.href = 'https://www.linkedin.com/in/suwon-jeong-225959171/'
 })
 
-let menuButton = document.getElementById('btn')
-menuButton.addEventListener('click', (event) => {
-  document.getElementById('mainText').style.display = 'none'
-  document.getElementById('mainImage').style.display = 'none'
-})
+// let menuButton = document.getElementById('btn')
+// menuButton.addEventListener('click', (event) => {
+//   document.getElementById('mainText').style.display = 'none'
+//   document.getElementById('mainImage').style.display = 'none'
+// })
 
-let noMenuButton = document.getElementById('cancel')
-noMenuButton.addEventListener('click', (event) => {
-  document.getElementById('mainText').style.display = 'block'
-  document.getElementById('mainImage').style.display = 'block'
-})
+// let noMenuButton = document.getElementById('cancel')
+// noMenuButton.addEventListener('click', (event) => {
+//   document.getElementById('mainText').style.display = 'block'
+//   document.getElementById('mainImage').style.display = 'block'
+// })
