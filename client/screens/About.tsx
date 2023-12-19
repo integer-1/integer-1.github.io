cc18b17e0981c4537bfd4927ffe39c5256a8a17a
Suwon Jeong <integer.won@gmail.com>
2023-12-19 15:35:38 +1300

    add cv download button

--- client/screens/About.tsx
+++ client/screens/About.tsx
@@ -1,23 +1,42 @@
 import MySkill from '../components/MySkill.tsx'
 
+const cvFileUrl = 'http://localhost:5173/Suwon_Jeong_CV.pdf'
+
 const About = () => {
+
+  const downloadPDF = (url: string) => {
+    const fileName = url.split('/').pop()
+    const aTag = document.createElement('a')
+
+    if (fileName === undefined) return <p>Sorry can not download CV</p>
+
+    aTag.href = url
+    aTag.setAttribute('download', fileName)
+    document.body.appendChild(aTag)
+    aTag.click()
+    aTag.remove()
+  }
+
   return (
     <div id="about" className="about">
       <p className="contact-title">About me</p>
 
       <div className="box">
         <p>
-          Hi, I&apos;m Suwon<br />
+          Hi, I&apos;m Suwon
+          <br />
           A business-savvy software developer <br />
           seasoned in business analysis and brand management.
         </p>
       </div>
-      
-
+      <button
+        onClick={() => {
+          downloadPDF(cvFileUrl)
+        }}
+      >download</button>
       <MySkill />
-      
-      <div className="end_line"></div>
 
+      <div className="end_line"></div>
     </div>
   )
 }
