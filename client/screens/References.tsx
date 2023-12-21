8e0070f4240cb32bdaae294b5ce89ba000d7cb5c
Suwon Jeong <integer.won@gmail.com>
2023-12-22 11:36:20 +1300

    change reference page style add css

--- client/screens/References.tsx
+++ client/screens/References.tsx
@@ -1,16 +1,26 @@
 import { Container, Row, Col } from 'react-bootstrap'
-import rabbit_png from '../images/rabbit.png'
+import reference_icon1 from '../images/reference_icon1.png'
+import reference_icon2 from '../images/reference_icon2.png'
+import reference_icon3 from '../images/reference_icon3.png'
 
 const referenceData = [
   {
     text: 'It was an absolute pleasure to work with Suwon. One of her many talents is that she’s great at communicating with others and coordinating multiple parties. Always willing to pull a lot more weight than expected is a plus. I would strongly recommend her to any future employer as a high performer with very quick response.',
     author: 'Siena Kim',
     position: 'UX & Digital content Team leader, Shinsegae',
+    icon: reference_icon1,
   },
   {
     text: 'Suwon is a talented and dedicated software developer. She was an asset to her dev teams while completing group work at Dev Academy and was unafraid to challenge herself with new technologies and ways of doing things. She is self-motivated and committed to producing high quality work.',
     author: 'John Gray',
     position: 'Bootcamp Facilitator, Dev Academy Aotearoa',
+    icon: reference_icon2,
+  },
+  {
+    text: 'Suwon is a very hard-working and conscientious developer. She is a joy to work with and is a talented problem-solver. Suwon is a curious learner and has a great attitude towards tackling coding challenges.',
+    author: 'Shrena Patel',
+    position: 'Bootcamp Facilitator, Dev Academy Aotearoa',
+    icon: reference_icon3,
   },
 ]
 
@@ -22,46 +32,26 @@ const References = () => {
         <Container>
           <div className="reference-container">
             <Row>
-              <Col xs={12} md={3} xl={3}>
-                <div className="circle">
-                  <img
-                    className="img-circle"
-                    src={rabbit_png}
-                    alt="reference1"
-                  />
-                </div>
-              </Col>
-              <Col xs={12} md={9} xl={9}>
-                <p className="reference1">
-                  {referenceData[0].text}
-                  <br />
-                  <br />
-                  <span>{referenceData[0].author}</span>
-                  <br />
-                  <span>{referenceData[0].position}</span>
-                </p>
-              </Col>
-            </Row>
-            <Row style={{ marginTop: '100px' }}>
-              <Col xs={12} md={9} xl={9}>
-                <p className="reference1">
-                  {referenceData[1].text}
-                  <br />
-                  <br />
-                  <span>{referenceData[1].author}</span>
-                  <br />
-                  <span>{referenceData[1].position}</span>
-                </p>
-              </Col>
-              <Col xs={12} md={3} xl={3}>
-                <div className="circle">
-                  <img
-                    className="img-circle"
-                    src={rabbit_png}
-                    alt="reference1"
-                  />
-                </div>
-              </Col>
+              {referenceData.map((data, index) => (
+                <Col key={index} xs={12} md={12} xl={4}>
+                  <div className="reference-box">
+                    <div className="circle">
+                      <img
+                        className="img-circle"
+                        src={data.icon}
+                        alt={`reference icon${index}`}
+                      />
+                    </div>
+                    <p className="reference-text">
+                      {data.text}
+                      <br />
+                      <br />
+                    </p>
+                    <p className="reference-author">{data.author}</p>
+                    <p className="reference-position">{data.position}</p>
+                  </div>
+                </Col>
+              ))}
             </Row>
           </div>
         </Container>
