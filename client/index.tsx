ea9b7792a290c6e1f80d3f045644543db98a5f42
Suwon Jeong <integer.won@gmail.com>
2024-02-01 13:38:37 +1300

    add root route for blog in index.tsx

--- client/index.tsx
+++ client/index.tsx
@@ -14,3 +14,11 @@ root.render(
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 )
+
+const root2 = createRoot(document.getElementById('blog') as HTMLElement)
+root2.render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+)
